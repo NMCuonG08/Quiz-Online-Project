33146280ad0ae6273c1f8c21d0a5ccf89c7713a0
NMCuong08 <NMCuong08>
2025-10-06 22:14:50 +0700

    feat(admin): add metadata quiz

--- app/admin/(categories)/quiz-categories/edit/[slug]/page.tsx
+++ app/admin/(categories)/quiz-categories/edit/[slug]/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import EditCategoryAdminPage from "@/modules/admin/pages/EditCategoryAdminPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Quiz Category",
+  description: "Edit Quiz Category",
+};
 
 const page = () => {
   return <EditCategoryAdminPage />;
